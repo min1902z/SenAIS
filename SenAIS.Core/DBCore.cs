acdc286d93cd5b8b84a5ddb8cb59c1e49322d2d3
min1902z <minholy1902z@gmail.com>
2025-07-21 10:01:08 +0700

    demo EF

---
+++ SenAIS.Core/DBCore.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SenAIS.Core
+{
+    public class DBCore
+    {
+        public void Test()
+        {
+            using (var context = new SenAISDBEntities())
+            {
+                var result = context.BrakeForces
+                                    .Where(v => v.SerialNumber == "12345")
+                                    .FirstOrDefault();
+                result.FrontLeftBrake = 10;
+                result.FrontRightBrake = 10;
+                context.SaveChanges();
+            }
+        }
+    }
+}
